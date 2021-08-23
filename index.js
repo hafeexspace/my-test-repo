3c11a26028b7b4e3a547cdf25e233369ab91a5e2
hafeexspace <hafeexspace@gmail.com>
2021-08-23 18:06:23 +0800

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+// Working with APIs
+// We need to answer the following question
+// what is the endpoint(url)
+// What query string/parameteres do we need to provide
+// what are required parameters
+// what optional paramaeters do we want to add
